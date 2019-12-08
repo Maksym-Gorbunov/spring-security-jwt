dc892af8f16a5c8ba13278e744f72491f6bc8a2b
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-12-08 23:09:03 +0100

    updated config

--- src/main/java/com/maxcoder/springsecurityjwt/MyController.java
+++ src/main/java/com/maxcoder/springsecurityjwt/MyController.java
@@ -1,57 +1,66 @@
 package com.maxcoder.springsecurityjwt;
 
-import com.maxcoder.springsecurityjwt.model.AuthenticationRequest;
-import com.maxcoder.springsecurityjwt.model.AuthenticationResponse;
-import com.maxcoder.springsecurityjwt.services.MyUserDetailsService;
+import com.maxcoder.springsecurityjwt.models.AuthenticationRequest;
+import com.maxcoder.springsecurityjwt.models.AuthenticationResponse;
 import com.maxcoder.springsecurityjwt.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-public class MyController {
+class MyController {
 
-  @Autowired
-  private AuthenticationManager authenticationManager;
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
-  @Autowired
-  private MyUserDetailsService userDetailsService;
+	@Autowired
+	private JwtUtil jwtTokenUtil;
 
-  @Autowired
-  private JwtUtil jwtUtil;
+	@Autowired
+  @Qualifier("MyUserDetailsService")
+	private UserDetailsService userDetailsService;
 
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
 
-  @RequestMapping("/hello")
-  public String hello(){
-    return "HELLO CONTROLLER";
-  }
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@Valid @RequestBody AuthenticationRequest authenticationRequest) throws Exception {
 
-  // Get username and password, return JWT (Json Web Token)
-  @RequestMapping(value = "/authentiacate", method = RequestMethod.POST)
-  public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
-    try{
-      authenticationManager.authenticate(
-              new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
-      );
-    }catch (BadCredentialsException e){
-      throw new Exception("Incorrect username or password", e);
-    }
-    final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+    System.out.println(authenticationRequest.toString());
 
-    final String jwt = jwtUtil.generateToken(userDetails);
+    System.out.println(authenticationRequest.getPassword());
+    System.out.println(authenticationRequest.getUsername());
+    try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+			);
+      System.out.println("111");
+		}
+		catch (BadCredentialsException e) {
+      System.out.println("000");
+			throw new Exception("Incorrect username or password", e);
+		}
+    System.out.println("222");
 
-    return ResponseEntity.ok(new AuthenticationResponse(jwt));
-  }
-
-
+		final UserDetails userDetails = userDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+    System.out.println("333");
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+    System.out.println("444");
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
 
 }
